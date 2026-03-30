1a3edea0b9b0a31c059011bb209d9d4c4ed2216b
João Victor <starssky395@gmail.com>
2026-03-30 17:16:31 -0300

    Add files via upload

---
+++ ex9/Fluxo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex9
+{
+    internal class Fluxo
+    {
+        public void Grama()
+        {
+            Console.WriteLine("Digite o seu nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite sua idade");
+            int idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Bem Vindo(a) "+nome +", anos "+idade);
+        }
+    }
+}
